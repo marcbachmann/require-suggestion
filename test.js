bfb51e85063bac4d696a52b2a4d12a602ede2b22
Marc Bachmann <marc.brookman@gmail.com>
2016-08-24 21:40:17 +0200

    Add standard linting

--- test.js
+++ test.js
@@ -4,14 +4,17 @@ require('./index')(__dirname)
 assert.doesNotThrow(function () { require('./package') })
 
 try {
-	require('./ackage')
+  require('./ackage')
 } catch (err) {
-	if (!/Error: Cannot find module '\.\/ackage'/.test(err.stack))
-		throw new Error('Stack does not begin with default error message')
+  if (!/Error: Cannot find module '\.\/ackage'/.test(err.stack)) {
+    throw new Error('Stack does not begin with default error message')
+  }
 
-	if (!/Probably you wanted to require one of those:/.test(err.stack))
-		throw new Error('Stack does not contain suggestion sentence')
+  if (!/Probably you wanted to require one of those:/.test(err.stack)) {
+    throw new Error('Stack does not contain suggestion sentence')
+  }
 
-	if (!/one of those:\n\n  \.\/package\.json/.test(err.stack))
-		throw new Error('Stack does not package.json suggestion')
+  if (!/one of those:\n\n/.test(err.stack) && !/\.\/package\.json/.test(err.stack)) {
+    throw new Error('Stack does not show package.json suggestion')
+  }
 }
